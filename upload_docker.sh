e13c01df1c04faf95ad142d7298efd1f4263cc35
carneiroDotDev <luiz@carneiro.dev>
2021-11-04 00:41:33 +0100

    Project  finished

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,7 +5,7 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=luuizpaulo/flaskapp
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +13,6 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker login -u luuizpaulo
+docker tag flaskapp $dockerpath:latest
+docker push $dockerpath:latest
